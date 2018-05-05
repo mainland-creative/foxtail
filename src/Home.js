be4a3836cec36d82b8fdd8992bc25c1eb6a2bf3a
Dan Hiester <dan.hiester@gmail.com>
2018-05-04 17:45:48 -0700

    First pass at video for mobile.

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,7 @@ import { getModel } from './CMS'
 
 import volume from './volume-up-4-xxl.png'
 import mute from './mute-2-xxl.png'
+import playButton from './play-button.svg'
 
 
 class Home extends Component {
@@ -16,25 +17,24 @@ class Home extends Component {
     super(props)
     this.state = {
       muted: true,
-      showVolume: false
+      showVolume: false,
+      videoPlaying: !this.videoRef.paused
     }
 
     this.getModel()
-    this.changeAudio = this.changeAudio.bind(this)
-    this.showVolume = this.showVolume.bind(this)
-    this.toggleVolume = this.toggleVolume.bind(this)
   }
+  videoRef = React.createRef()
 
   async getModel () {
     const model = await getModel('65u5lkjGCW2e2W48ISgg6C')
     this.setState({ data: model.items[0].fields })
   }
 
-  changeAudio () {
+  changeAudio = () => {
     this.setState({ muted: !this.state.muted })
   }
 
-  showVolume () {
+  showVolume = () => {
     if (this.state.showVolume) {
       return ''
     } else {
@@ -42,8 +42,25 @@ class Home extends Component {
     }
   }
 
-  toggleVolume () {
+  toggleVolume = () => {
+    const showVolume = this.state.showVolume
+    console.log('showVolume = ', showVolume)
     this.setState({ showVolume : !this.state.showVolume })
+  }
+
+  toggleVideoPlayback = (e) => {
+    const video = this.videoRef.current
+    if (video.paused) {
+      video.play()
+      this.setState({
+        videoPlaying: true
+      })
+    } else {
+      video.pause()
+      this.setState({
+        videoPlaying: false
+      })
+    }
   }
 
   render () {
@@ -53,14 +70,27 @@ class Home extends Component {
     const videoStyle = {
       backgroundSize: 'cover',
       backgroundPosition: 'center',
+      cursor: 'pointer',
+      position: 'relative',
       height: document.documentElement.clientHeight - 65,
       width: document.documentElement.clientWidth
+    }
+    const playButtonStyle  = {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      opacity: '.5',
+      zIndex: '5',
+      transform: 'translate(-50%, -50%)',
+      pointerEvents: 'none',
+      display: this.state.videoPlaying ? 'none' : 'block'
     }
 
     return (
       <div>
         <div onMouseEnter={this.toggleVolume} onMouseLeave={this.toggleVolume} style={videoStyle} className="Home">
-          <video style={ {...videoStyle, paddingTop: '0px', overflow: 'hidden' }} src={foxtailVideo.fields.file.url} poster={videoPoster.fields.file.url} loop muted={this.state.muted} autoPlay />
+          <img src={playButton} alt='Toggle Playback' style={playButtonStyle} />
+          <video style={ {...videoStyle, paddingTop: '0px', overflow: 'hidden' }} src={foxtailVideo.fields.file.url} poster={videoPoster.fields.file.url} loop muted={this.state.muted} autoPlay ref={this.videoRef} onClick={this.toggleVideoPlayback} />
           <div onClick={this.changeAudio} className={`SoundIcon${this.showVolume()}`}>
             <img style={{ height: '30px', width: '30px' }} src={this.state.muted ? mute : volume }></img>
           </div>
